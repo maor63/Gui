06a5736f8ce558b5cec5f4eb2549394be51a5356
Tal Ivanov <talivanov@gmail.com>
2018-01-15 21:42:09 +0200

    Checking input from value

--- src/View/SearchView/SearchViewController.java
+++ src/View/SearchView/SearchViewController.java
@@ -1,6 +1,7 @@
 package View.SearchView;
 
 import View.AbstractController;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
@@ -18,8 +19,27 @@ public class SearchViewController extends AbstractController {
     public TextField Street;
 
     public void serachProducts(MouseEvent mouseEvent) {
+        if(inValidInput())
+        {
+            showMessage("Invalid input");
+            return;
+        }
+
         LocalDate startDateValue = start_date.getValue();
         LocalDate endDateValue = end_date.getValue();
         viewModel.searchPackagesBy(startDateValue, endDateValue);
+    }
+
+    private boolean inValidInput() {
+        return start_date == null || end_date == null || City.getText().isEmpty();
+    }
+
+    private void showMessage(String message)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error Dialog");
+        alert.setHeaderText(message);
+
+        alert.showAndWait();
     }
 }
