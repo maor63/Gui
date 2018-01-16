9d27f19b76035d596074569ad8093699ef6bc6de
maorreu <sdfsdf>
2018-01-16 17:41:20 +0200

    Alon serch address

--- src/Main/ViewModel.java
+++ src/Main/ViewModel.java
@@ -30,10 +30,18 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author proxc
@@ -338,5 +346,42 @@ public class ViewModel extends Application
 
     public User getUser() {
         return user;
+    }
+
+    public void sendEmail(String Email) throws MessagingException {
+        String host = "smtp.gmail.com";
+        String user = "everything4rent4@gmail.com";
+        String pass = "nituz123";
+        String to = Email;
+        String from = "everything4rent4@gmail.com";
+        String subject = "Welcome to Everything4Rent";
+        String message = "You are now a member of Everything4Rent system";
+        boolean sessionDebug = false;
+
+
+        Properties props = System.getProperties();
+
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.required", "true");
+
+        java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        Session mailSession = Session.getDefaultInstance(props, null);
+        mailSession.setDebug(sessionDebug);
+        Message msg = new MimeMessage(mailSession);
+        msg.setFrom(new InternetAddress(from));
+        InternetAddress[] address = {new InternetAddress(to)};
+        msg.setRecipients(Message.RecipientType.TO, address);
+        msg.setSubject(subject); msg.setSentDate(new Date());
+        msg.setText(message);
+
+        Transport transport=mailSession.getTransport("smtp");
+        transport.connect(host, user, pass);
+        transport.sendMessage(msg, msg.getAllRecipients());
+        transport.close();
+        System.out.println("message send successfully");
+
     }
 }
